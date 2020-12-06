8519fce49486e03f6c584609e8d9c745c606d52e
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-12-06 21:19:06 +0300

    Added DbContext

--- PlannerAPI/Models/InfoPlanner.cs
+++ PlannerAPI/Models/InfoPlanner.cs
@@ -7,5 +7,16 @@ namespace PlannerAPI
 {
     public class InfoPlanner
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime startTodo { get; set; }
+
+        public DateTime dueTodo { get; set; }
+
+        public bool Completed { get; set; }
     }
 }
